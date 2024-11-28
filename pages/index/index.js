ee62bbf88f576bbc130e1a9103f062f52c551c7d
PikachusEar <jhexzengzhaoming@163.com>
2024-11-27 23:56:50 -0500

    isActivated function update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,8 @@
 Page({
   data: {
     banners: [],
-    news: []
+    news: [],
+    is_activated: false  // 默认设置为 false
   },
 
   onLoad: function() {
@@ -11,13 +12,17 @@ Page({
   fetchData: function() {
     var that = this;
     wx.request({
-      url: 'https://boomspace.acornyun.com/api/get_images',  // 根据实际部署的后端URL修改
+      url: 'http://localhost:8000/api/get_images',
       method: 'GET',
       success: function(res) {
         if (res.statusCode === 200) {
+          // 假设使用第一张图片的 is_activated 值
+          const is_activated = res.data.images.length > 0 ? res.data.images[0].is_activated : false;
+          
           that.setData({
             banners: res.data.images,
-            news: res.data.news
+            news: res.data.news,
+            is_activated: is_activated  // 设置 is_activated 的值
           });
         } else {
           console.log('数据加载失败:', res);
